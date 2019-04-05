e067cd663de3276b7bc931bb68378081698ef5f1
Randolph Davis Hill <rdhillbb@icloud.com>
2019-04-05 20:18:21 +0800

    Todo list complete with datasource and delegate methods.

---
+++ DoMeToday/ToDoListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  DoMeToday
+//
+//  Created by Randolph Davis Hill on 1/4/19.
+//  Copyright © 2019 Randolph Davis Hill. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //Mark - Tableview DataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    //Mark - TableView Delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // print("Row: \(indexPath.row) item:" + itemArray[indexPath.row])
+        //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
